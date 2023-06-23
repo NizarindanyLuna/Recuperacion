13a2f3fc137735639d07f0d9fef089e4688f6996
Nizarindany González Luna <nizikyua.18@gmail.com>
2023-06-23 17:25:58 -0600

    "Inicio Proyecto"

---
+++ src/main/java/Controlador/Controlador.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Controlador;
+
+/**
+ *
+ * @author Is Dahud
+ */
+public class Controlador {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
